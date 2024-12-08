0b1a4b86da2487ee529c6452d7f75c588df0aebf
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-12-08 21:49:13 +0700

    refactor: add impl_chain_method macro

--- core/src/rpc.rs
+++ core/src/rpc.rs
@@ -28,16 +28,14 @@ impl RpcProvider {
 	pub async fn new(chain: &str, url: &str) -> Result<RpcProvider> {
 		info!("Initialization with url: {:?}", url);
 		match chain {
-			c if c == "solana" => {
-				Ok(RpcProvider { inner: Inner::Solana(SolanaRpcProvider::new(&url)?) })
-			},
+			c if c == "solana" =>
+				Ok(RpcProvider { inner: Inner::Solana(SolanaRpcProvider::new(&url)?) }),
 			c if c == "substrate" => {
 				let provider = SubstrateRpcProvider::new(&url).await?;
 				Ok(RpcProvider { inner: Inner::Substrate(provider) })
 			},
-			c if c == "ethereum" => {
-				Ok(RpcProvider { inner: Inner::Ethereum(EthereumRpcProvider::new(&url)?) })
-			},
+			c if c == "ethereum" =>
+				Ok(RpcProvider { inner: Inner::Ethereum(EthereumRpcProvider::new(&url)?) }),
 			_ => Err(eyre!("Unsupported chain")),
 		}
 	}
@@ -45,11 +43,20 @@ impl RpcProvider {
 
 impl OnchainRpcProvider for RpcProvider {
 	async fn get_block_number(&self) -> Result<u64> {
-		match &self.inner {
-			Inner::Solana(inner) => inner.get_block_number().await,
-			Inner::Ethereum(inner) => inner.get_block_number().await,
-			Inner::Substrate(inner) => inner.get_block_number().await,
-			Inner::Unknown(inner) => inner.get_block_number().await,
-		}
+		impl_chain_method!(self, (Solana, Ethereum, Substrate)::get_block_number)
 	}
+}
+
+/// Helper macro to match and execute the provided method without repeating the redundant variants
+/// in pattern matching.
+#[macro_export]
+macro_rules! impl_chain_method {
+	($self:ident, ($($variant:ident),*)::$method:ident) => {
+		match &$self.inner {
+			$(
+				Inner::$variant(inner_value) => inner_value.$method().await,
+			)*
+			_ => unimplemented!(),
+		}
+	};
 }
